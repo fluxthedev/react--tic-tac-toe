870f23697eba84c71912dbb01f5e6069b49e35fa
John Ore <jore@ofbf.org>
2021-12-09 13:04:05 -0500

    writing test for simulating a winning game

--- src/Components/Game/game.test.js
+++ src/Components/Game/game.test.js
@@ -16,9 +16,32 @@ it('renders game status for first player', () => {
     expect(firstPlayer).toEqual('Next player: X');
 });
 
-it('renders game status for second playerr', () => {
+it('renders game status for second player', () => {
     const button = wrapper.find('button.square').first();
     button.simulate('click');
     const secondPlayer = wrapper.find('div.game-info').children().first().text();
     expect(secondPlayer).toEqual('Next player: O');
+});
+
+it('simulates game with winner', () => {
+    const turnTwo = wrapper.find('button.square').at(2);
+    turnTwo.simulate('click');
+
+    const turnThree = wrapper.find('button.square').at(4);
+    turnThree.simulate('click');
+
+    const turnFour = wrapper.find('button.square').at(8);
+    turnFour.simulate('click');
+
+    const turnFive = wrapper.find('button.square').at(5);
+    turnFive.simulate('click');
+
+    const turnSix = wrapper.find('button.square').at(1);
+    turnSix.simulate('click');
+
+    const turnSeven = wrapper.find('button.square').at(3);
+    turnSeven.simulate('click');
+
+    const winnerStatus = wrapper.find('div.game-info').children().first().text();
+    expect(winnerStatus).toEqual('Winner: X');
 });
