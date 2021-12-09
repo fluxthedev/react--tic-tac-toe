bda9fe38497d548cd99bb76edef2987dc4880482
John Ore <jore@ofbf.org>
2021-12-09 11:22:27 -0500

    Test to render game status for second player

--- src/Components/Game/game.test.js
+++ src/Components/Game/game.test.js
@@ -2,12 +2,23 @@ import React from "react";
 import Game from './game';
 import { shallow, mount } from "enzyme";
 
+let wrapper;
+beforeEach(() => {
+    wrapper = mount(<Game/>);
+});
+
 it('Game component renders without crashing', () => {
     shallow(<Game />);
 });
 
-it('renders game info correctly', () => {
-    const wrapper = mount(<Game/>);
+it('renders game status for first player', () => {
     const firstPlayer = wrapper.find('div.game-info').children().first().text();
     expect(firstPlayer).toEqual('Next player: X');
+});
+
+it('renders game status for second playerr', () => {
+    const button = wrapper.find('button.square').first();
+    button.simulate('click');
+    const secondPlayer = wrapper.find('div.game-info').children().first().text();
+    expect(secondPlayer).toEqual('Next player: 0');
 });
